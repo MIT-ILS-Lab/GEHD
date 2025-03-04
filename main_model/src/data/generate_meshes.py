cec48a602e01e4ff15209964efc2a57f0dcec04b
Damian Gerber <damian.gerber21@imperial.ac.uk>
2025-03-04 12:22:22 -0500

    adjusting submodule

--- main_model/src/data/generate_meshes.py
+++ main_model/src/data/generate_meshes.py
@@ -1,21 +1,27 @@
 import os
 import trimesh
-import sys
-
-print("System path: %s" % sys.path)
 
 from gegnn.utils.thsolver import default_settings
+from gegnn.utils.thsolver.config import parse_args
+
+import inspect
+
+# Get the file path of the function definition
+file_path = inspect.getfile(parse_args)
+
+# Print the path
+print(f"The function 'parse_args' is defined in: {file_path}")
 
 # initialize global settings
 default_settings._init()
-from gegnn.utils.thsolver.config import parse_args
+FLAGS = parse_args(config_path="main_model/config.yaml")
 
 if __name__ == "__main__":
-    FLAGS = parse_args(config_path="main_model/config.yaml")
+
     default_settings.set_global_values(FLAGS)
 
     # Define path to mesh directory
-    PATH_TO_MESH = "main_model/disk/meshes"
+    PATH_TO_MESH = FLAGS.DATA.preparation.path_to_mesh
 
     # Ensure the directory exists
     os.makedirs(PATH_TO_MESH, exist_ok=True)
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(name="thesis_dgerber", version="0.1.0", packages=find_packages())
+setup(
+    name="thesis_dgerber", version="0.1.0", packages=find_packages("gegnn", "gegnn.*")
+)
